fc6f7ffd74787fad53f29a4aefb32f2d41259bc8
Zomebie <dmsqlcjstk77@naver.com>
2026-03-12 16:51:13 +0900

    refactor: remove Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import styled, { createGlobalStyle } from "styled-components";
-import Header from "./components/Header";
 import ConnectWallet from "./components/ConnectWallet";
 import GetAddress from "./components/GetAddress";
 import GetBalance from "./components/GetBalance";
@@ -29,6 +28,27 @@ const Page = styled.div`
   padding-bottom: 60px;
 `;
 
+const HeaderWrapper = styled.header`
+  padding: 40px 20px 32px;
+  max-width: 680px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  margin: 0;
+  color: #1d1d1f;
+  letter-spacing: -0.03em;
+`;
+
+const Subtitle = styled.p`
+  font-size: 14px;
+  color: #86868b;
+  margin: 6px 0 0;
+  font-weight: 400;
+`;
+
 const Container = styled.div`
   max-width: 680px;
   margin: 0 auto;
@@ -46,7 +66,10 @@ export default function App() {
     <>
       <GlobalStyle />
       <Page>
-        <Header />
+        <HeaderWrapper>
+          <Title>Adena Wallet Integration</Title>
+          <Subtitle>Connect and interact with Gno.land blockchain</Subtitle>
+        </HeaderWrapper>
         <Container>
           <CardList>
             <ConnectWallet />
